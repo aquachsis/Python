f171be16522e7c578238a3f23648ddeb34d4d159
Alan Quach <aquach@sis.us>
2018-10-28 16:23:28 -0700

    Updated to catch errors

--- check_stub_parse.py
+++ check_stub_parse.py
@@ -1,25 +1,27 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-src_file = open('check_stub.PDF', 'rb')
+src_file = open('check_stubs.PDF', 'rb')
 reader = PdfFileReader(src_file)
 employee_id_chars = 5
 
-for i in range(2):
+for i in range(reader.getNumPages()):
     page = reader.getPage(i)
     page_text = page.extractText()
-    employee_id_substring = page_text.split('Employee ID\n')[1]
-    employee_id = employee_id_substring[:employee_id_chars]
-
-    print('Page: ' + str(i + 1))
-    print('Employee ID: ' + employee_id)
-    print('\n')
-
-    pdf_writer = PdfFileWriter()
-    pdf_writer.addPage(page)
-    with open('%d.pdf' % int(employee_id), 'wb') as file:
-        pdf_writer.write(file)
-# page = reader.getPage(0)
-# page_text = page.extractText()
-# print(page_text)
-
+    if i == 0:
+        employee_id_substring = page_text.split('Employee ID\n')[1]
+        employee_id = employee_id_substring[:employee_id_chars]
+        print('Page: ' + str(i) + ' -- Employee ID: ' + employee_id + '\n')
+        pdf_writer = PdfFileWriter()
+        pdf_writer.addPage(page)
+    else:
+        try:
+            employee_id_substring = page_text.split('Employee ID\n')[1]
+            with open(str(i) + ' - %d.pdf' % int(employee_id), 'wb') as file:
+                pdf_writer.write(file)
+            employee_id = employee_id_substring[:employee_id_chars]
+            print('Page: ' + str(i) + ' -- Employee ID: ' + employee_id + '\n')
+            pdf_writer = PdfFileWriter()
+            pdf_writer.addPage(page)
+        except IndexError:
+            pdf_writer.addPage(page)
 src_file.close()
